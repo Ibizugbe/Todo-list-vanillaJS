c0c363841fdb4b0a1901bcf86be72ce8ef66a96b
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-07 09:12:58 +0100

    target input and button element

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 // eslint-disable-next-line no-unused-vars
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
+
+const todoInput = document.querySelector(".todo-input");
+const todoBtn = document.querySelector(".todo-btn");
+const myList = document.querySelector(".todo-list");
 
 const todoList = [
   {
-    description: 'Wash the clothes',
+    description: "Wash the clothes",
     completed: true,
     index: 1,
   },
 
   {
-    description: 'Complete the To Do list task',
+    description: "Complete the To Do list task",
     completed: false,
     index: 2,
   },
 
   {
-    description: 'Shop for food items',
+    description: "Shop for food items",
     completed: true,
     index: 3,
   },
 ];
 
 todoList.forEach((element) => {
-  const myList = document.querySelector('.todo-list');
-  const lists = document.createElement('li');
-  lists.classList.add('todo-li');
+  const lists = document.createElement("li");
+  lists.classList.add("todo-li");
   lists.innerHTML = `
     <div class="description">
       <input type="checkbox" />
