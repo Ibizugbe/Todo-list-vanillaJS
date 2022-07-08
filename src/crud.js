18b6fa72dd67b21b09dd9bad4129404710e6b5cf
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-08 08:20:29 +0100

    delete crudjs

--- src/crud.js
+++
@@ -1,8 +0,0 @@
-// const todoBtn = document.getElementsByClassName(".todo-btn")[0];
-// const myList = document.querySelector(".todo-list");
-// const todoInput = document.querySelector(".todo-input")[0];
-// const tasks = JSON.parse(localStorage.getItem("task")) || [];
-
-// todoBtn.addEventListener("click", function hello() {
-//   console.log(123);
-// });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 // eslint-disable-next-line no-unused-vars
 import _ from "lodash";
 import "./style.css";
-
-// import * as crud from "./crud.js";
-
-// crud();
-// const todoBtn = document.getElementsByClassName(".todo-btn")[0];
-// todoBtn.addEventListener("click", sayHello);
-// function sayHello() {
-//   console.log(123);
-// }
-console.log(123);
