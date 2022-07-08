12d59fe748da1447443a641aa9761c086ace7027
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-08 14:54:02 +0100

    fix linter errors

--- src/crud.js
+++ src/crud.js
@@ -1,19 +1,20 @@
-import { clearList, clearAll } from "./functions.js";
-const myList = document.querySelector(".todo-list");
-const todoBtn = document.querySelector(".todo-btn");
-const todoInput = document.querySelector(".todo-input");
-const clearBtn = document.querySelector(".clear-btn");
+import { clearList, clearAll } from './functions.js';
+
+const myList = document.querySelector('.todo-list');
+const todoBtn = document.querySelector('.todo-btn');
+const todoInput = document.querySelector('.todo-input');
+const clearBtn = document.querySelector('.clear-btn');
 
 let taskList = [];
 
 const display = () => {
-  myList.innerHTML = "";
-  taskList = JSON.parse(localStorage.getItem("localItem")) || [];
+  myList.innerHTML = '';
+  taskList = JSON.parse(localStorage.getItem('localItem')) || [];
   taskList.forEach((task) => {
-    const myList = document.querySelector(".todo-list");
-    const flag = task.completed ? "checked" : "";
-    const lists = document.createElement("li");
-    lists.classList.add("todo-li");
+    const myList = document.querySelector('.todo-list');
+    const flag = task.completed ? 'checked' : '';
+    const lists = document.createElement('li');
+    lists.classList.add('todo-li');
     lists.innerHTML = `
       <div class="description">
         <input type="checkbox" ${flag} class="checkbox" data-set="${task.index}"/>
@@ -25,23 +26,23 @@ const display = () => {
     `;
     myList.appendChild(lists);
     const taskInput = lists.children[1];
-    taskInput.addEventListener("change", () => {
-      const taskListChores = document.querySelector(".todo-li");
+    taskInput.addEventListener('change', () => {
+      const taskListChores = document.querySelector('.todo-li');
       const array = Array.from(taskListChores.children);
       const index = array.indexOf(lists);
-      const taskListLocal = JSON.parse(localStorage.getItem("localItem"));
+      const taskListLocal = JSON.parse(localStorage.getItem('localItem'));
       taskListLocal[index].description = taskInput.value;
-      localStorage.setItem("localItem", JSON.stringify(taskListLocal));
+      localStorage.setItem('localItem', JSON.stringify(taskListLocal));
     });
   });
 };
 
 display();
 
-todoBtn.addEventListener("click", (e) => {
+todoBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  if (todoInput.value === "") return;
-  taskList = JSON.parse(localStorage.getItem("localItem")) || [];
+  if (todoInput.value === '') return;
+  taskList = JSON.parse(localStorage.getItem('localItem')) || [];
   const obj = {
     description: todoInput.value,
     completed: false,
@@ -49,8 +50,8 @@ todoBtn.addEventListener("click", (e) => {
   };
 
   taskList.push(obj);
-  localStorage.setItem("localItem", JSON.stringify(taskList));
-  todoInput.value = "";
+  localStorage.setItem('localItem', JSON.stringify(taskList));
+  todoInput.value = '';
   display();
 });
 
@@ -63,19 +64,19 @@ const removeTask = (index) => {
     i += 1;
   });
   taskList.push(...newArr);
-  localStorage.setItem("localItem", JSON.stringify(taskList));
+  localStorage.setItem('localItem', JSON.stringify(taskList));
   display();
 };
-myList.addEventListener("click", (e) => {
-  if (e.target.classList.contains("fa-solid")) {
-    const index = parseInt(e.target.getAttribute("id"), 10);
+myList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-solid')) {
+    const index = parseInt(e.target.getAttribute('id'), 10);
     removeTask(index);
   }
 });
 
-myList.addEventListener("click", clearList);
+myList.addEventListener('click', clearList);
 
-clearBtn.addEventListener("click", (e) => {
+clearBtn.addEventListener('click', (e) => {
   e.preventDefault();
   clearAll();
   display();
--- src/functions.js
+++ src/functions.js
@@ -1,15 +1,15 @@
 export const clearList = (e) => {
-  const clicked = e.target.closest(".checkbox");
+  const clicked = e.target.closest('.checkbox');
   if (!clicked) return;
-  const targetData = parseInt(clicked.getAttribute("data-set"), 10);
-  const task = JSON.parse(localStorage.getItem("localItem")) || [];
+  const targetData = parseInt(clicked.getAttribute('data-set'), 10);
+  const task = JSON.parse(localStorage.getItem('localItem')) || [];
   const update = task.find((todo) => todo.index === targetData);
   update.completed = !update.completed;
-  localStorage.setItem("localItem", JSON.stringify(task));
+  localStorage.setItem('localItem', JSON.stringify(task));
 };
 
 export const clearAll = () => {
-  const setting = JSON.parse(localStorage.getItem("localItem")) || [];
+  const setting = JSON.parse(localStorage.getItem('localItem')) || [];
   const notCompleted = setting.filter((taskList) => !taskList.completed);
   setting.length = 0;
   let i = 0;
@@ -19,5 +19,5 @@ export const clearAll = () => {
   });
 
   setting.push(...notCompleted);
-  localStorage.setItem("localItem", JSON.stringify(setting));
+  localStorage.setItem('localItem', JSON.stringify(setting));
 };
