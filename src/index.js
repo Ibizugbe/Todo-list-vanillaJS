3c6d86456712845ed83c116dc958168c96843bbd
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-08 08:07:29 +0100

    add crudjs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
 import _ from "lodash";
 import "./style.css";
 
-import * as crud from "./crud.js";
+// import * as crud from "./crud.js";
 
-crud();
+// crud();
+// const todoBtn = document.getElementsByClassName(".todo-btn")[0];
+// todoBtn.addEventListener("click", sayHello);
+// function sayHello() {
+//   console.log(123);
+// }
+console.log(123);
